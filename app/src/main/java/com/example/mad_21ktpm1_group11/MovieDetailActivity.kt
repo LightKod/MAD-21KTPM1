a7c60dcf669f9ed445c8462855773a8a14532a5e
Quang <nguyennhatquangkod2003@gmail.com>
2024-03-28 19:43:03 +0700

    movie detail + seat selection frame

---
+++ app/src/main/java/com/example/mad_21ktpm1_group11/MovieDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_21ktpm1_group11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
---
+++ app/src/main/java/com/example/mad_21ktpm1_group11/SeatSelectionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_21ktpm1_group11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeatSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_selection)
+    }
+}
