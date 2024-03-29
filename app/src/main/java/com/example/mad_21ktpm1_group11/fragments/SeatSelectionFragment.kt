eab983076def3fdc29057e646e830016dbc18e4f
LightKod <nguyennhatquangkod2003@gmail.com>
2024-03-29 10:04:09 +0700

    Fix Ignore + convert Activity to Fragment

---
+++ app/src/main/java/com/example/mad_21ktpm1_group11/fragments/SeatSelectionFragment.kt
@@ -0,0 +1,19 @@
+package com.example.mad_21ktpm1_group11.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mad_21ktpm1_group11.R
+
+class SeatSelectionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_seat_selection, container, false)
+    }
+}
